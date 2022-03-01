2b33d9e17119994bd20b77d38c3e9f1b76dc52ab
Zachary Rupprecht <99833357+Metype-DevCat@users.noreply.github.com>
2022-03-01 12:32:19 -0600

    Make website multi-page! :D

---
+++ script.js
@@ -0,0 +1,24 @@
+function populateHeader(unlinked) {
+    let links = ["index.html","page2.html"]
+  
+    let names = ["Main","Page2"]
+  
+    let header = $("<header></header>");
+    header.addClass("header");
+    header.append($("<h1></h1>").html("Header"))
+    let ul = $("<ul></ul>")
+    header.append(ul);
+    for(let i in links){
+      let a = $("<a></a>")
+      ul.append($("<li></li>").addClass("nav").append(a.addClass("nav")));
+      if(unlinked == links[i]){
+        a.addClass("current");
+      }else{
+        a.attr("href",links[i])
+      }
+      a.html("<h1>"+names[i]+"</h1>");
+    }
+    $("body").prepend(header);
+  }
+  
+  
